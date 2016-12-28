51e49c0ad4167dd2c3a793146cd38cc17bc1684e
Léo LE BRAS <leo.lebrasf@gmail.com>
2016-12-29 00:27:35 +0100

    fix(Navigation): export component

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ export { default as BackButton } from './modules/BackButton'
 export { default as BottomNavigation } from './modules/BottomNavigation'
 export { default as NavBar } from './modules/NavBar'
 export { default as Navigator } from './modules/Navigator'
+export { default as Navigation } from './modules/Navigation'
 export { default as TabBarBottom } from './modules/TabBarBottom'
 export { default as Tabs } from './modules/Tabs'
 
@@ -14,5 +15,5 @@ export { default as NativeRenderer } from './modules/NativeRenderer'
 export { default as TabStack } from './modules/TabStack'
 export { default as TabViewPagerNavigator } from './modules/TabViewPagerNavigator'
 
-// Util for creating who-knows-what!
-export * as utils from './modules/utils'
+// TypeDefinitions
+// @TODO
