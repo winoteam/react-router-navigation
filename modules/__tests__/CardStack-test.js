fba0b3bcf07cf69758f2512f57e45fc5b1a7ee7c
Léo LE BRAS <leo.lebrasf@gmail.com>
2016-12-30 18:54:20 +0100

    tes(CardStack): add onNavigateBack method

--- modules/__tests__/CardStack-test.js
+++ modules/__tests__/CardStack-test.js
@@ -1,5 +1,5 @@
-import 'react-native'
-import React, { PropTypes, Component } from 'react'
+import { View } from 'react-native'
+import React, { PropTypes, Component, createElement } from 'react'
 import { Match } from 'react-router'
 import { TestRouter, componentFactory, CardView } from './utils'
 import CardStack from './../CardStack'
@@ -66,6 +66,31 @@ it('<CardStack /> re-renders correctly when "pop" action is called', () => {
   let tree = component.toJSON()
   expect(tree).toMatchSnapshot()
   tree.props.onPress((history) => history.goBack())
+  tree = component.toJSON()
+  expect(tree).toMatchSnapshot()
+})
+
+it('<CardStack /> re-renders correctly when onNavigateBack() method is called', () => {
+  const component = renderer.create(
+    <TestRouter
+      initialIndex={1}
+      initialEntries={['/', '/hello']}
+    >
+      <CardStack render={({ navigationState, onNavigateBack, cards }) => {
+        return (
+          <View onPress={onNavigateBack}>
+            {CardView({ navigationState, onNavigateBack, cards })}
+          </View>
+        )
+      }}>
+        <Match exactly pattern="/" component={componentFactory('Index')} />
+        <Match pattern="/hello" component={componentFactory('Hello')} />
+      </CardStack>
+    </TestRouter>
+  )
+  let tree = component.toJSON()
+  expect(tree).toMatchSnapshot()
+  tree.children[0].children[0].props.onPress()
   tree = component.toJSON()
   expect(tree).toMatchSnapshot()
 })
