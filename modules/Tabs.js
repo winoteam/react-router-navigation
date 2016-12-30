05e60ab6d6211d849acc81368561679dddaddae4
Léo LE BRAS <leo.lebrasf@gmail.com>
2016-12-30 13:55:52 +0100

    feat(Tabs): works with <TabStack />

--- modules/Tabs.js
+++ modules/Tabs.js
@@ -1,17 +1,59 @@
 /* @flow */
 
-import React from 'react'
+import React, { createElement } from 'react'
+import { StyleSheet, Dimensions, Text } from 'react-native'
+import { TabViewAnimated, TabBarTop } from 'react-native-tab-view'
 import TabStack from './TabStack'
-import TabBarBottom from './TabBarBottom'
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  wrapper: {
+    flex: 1,
+  },
+  tabLabel: {
+    backgroundColor: 'transparent',
+    color: 'white',
+    margin: 8,
+  },
+})
 
 type Props = {
-  children: Array<React$Element<any>>,
+  containerStyle?: StyleSheet,
+  style?: StyleSheet,
 }
 
-const Tabs = (props: Props): React$Element<any> => (
+const Tabs = (props: any): React$Element<any> => (
   <TabStack
     {...props}
-    renderHeader={TabBarBottom}
+    style={[styles.container, props.containerStyle]}
+    render={({ tabs, navigationState, onRequestChangeTab }) => (
+      <TabViewAnimated
+        style={[styles.container, props.style]}
+        initialLayout={Dimensions.get('window')}
+        navigationState={navigationState}
+        onRequestChangeTab={onRequestChangeTab}
+        renderHeader={(sceneProps) => (
+          <TabBarTop
+            {...sceneProps}
+            renderLabel={({ route }) => {
+              const scene = tabs.find((tab) => tab.key === route.key)
+              if (!scene.title) return null
+              return (
+                <Text style={styles.tabLabel}>
+                  {scene.title}
+                </Text>
+              )
+            }}
+          />
+        )}
+        renderScene={({ route }) => {
+          const scene = tabs.find((tab) => tab.key === route.key)
+          return scene ? createElement(scene.component) : null
+        }}
+      />
+    )}
   />
 )
 
