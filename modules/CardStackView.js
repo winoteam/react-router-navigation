8ae87f79d914968f1287f343a71c0f0fe1558fa5
Léo LE BRAS <leo.lebrasf@gmail.com>
2017-01-01 23:22:06 +0100

    fix(CardStack): introduce <CardStackView />

---
+++ modules/CardStackView.js
@@ -0,0 +1,28 @@
+/* @flow */
+
+import { Component } from 'react'
+import _ from 'lodash'
+import type { NavigationTransitionProps } from 'react-native/Libraries/NavigationExperimental/NavigationTypeDefinition'
+import type { Card } from './StackTypeDefinitions'
+
+type Props = NavigationTransitionProps & {
+  cards: Array<Card>,
+  onNavigateBack: Function,
+}
+
+class CardStackView extends Component<void, Props, void> {
+
+  shouldComponentUpdate(nextProps: Props): boolean {
+    return !_.isEqual(
+      this.props.navigationState,
+      nextProps.navigationState,
+    )
+  }
+
+  render() {
+    return this.props.render(this.props)
+  }
+
+}
+
+export default CardStackView
