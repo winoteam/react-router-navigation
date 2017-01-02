46a66d31d5081170456c42c693eddc95a005f07c
Léo LE BRAS <leo.lebrasf@gmail.com>
2017-01-02 10:54:15 +0100

    fix(*): load new modules

--- index.js
+++ index.js
@@ -2,12 +2,14 @@
 
 // High-level wrappers
 export { default as BackButton } from './modules/BackButton'
+export { default as BottomNavigation } from './modules/BottomNavigation'
+export { default as Link } from './modules/Link'
 export { default as MatchCard } from './modules/MatchCard'
 export { default as MatchTab } from './modules/MatchTab'
 export { default as NavBar } from './modules/NavBar'
 export { default as Navigation } from './modules/Navigation'
-export { default as TabBarBottom } from './modules/TabBarBottom'
 export { default as Tabs } from './modules/Tabs'
+export { default as withHistory } from './modules/withHistory'
 
 // Low-level building blocks
 export { default as CardStack } from './modules/CardStack'
