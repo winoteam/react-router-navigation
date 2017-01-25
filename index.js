b096847d98a49ce9c512ea7fb8322a3536aa6d39
Léo LE BRAS <leo.lebrasf@gmail.com>
2017-01-25 22:23:18 +0100

    feat(index): export all modules

--- index.js
+++ index.js
@@ -3,17 +3,21 @@
 // High-level wrappers
 export { default as BackButton } from './modules/BackButton'
 export { default as BottomNavigation } from './modules/BottomNavigation'
+export { default as BottomNavigationBar } from './modules/BottomNavigationBar'
 export { default as Link } from './modules/Link'
 export { default as MatchCard } from './modules/MatchCard'
 export { default as MatchTab } from './modules/MatchTab'
 export { default as NavBar } from './modules/NavBar'
 export { default as Navigation } from './modules/Navigation'
+export { default as Navigator } from './modules/Navigator'
 export { default as Tabs } from './modules/Tabs'
 export { default as withHistory } from './modules/withHistory'
 
 // Low-level building blocks
 export { default as CardStack } from './modules/CardStack'
+export { default as NativeRenderer } from './modules/NativeRenderer'
 export { default as TabStack } from './modules/TabStack'
+export { default as TabViewPagerNavigator } from './modules/TabViewPagerNavigator'
 
 // TypeDefinitions
 export type { Card, Tab } from './modules/StackTypeDefinitions'
