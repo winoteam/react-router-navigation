4b43a527ac4a6d18cbfbf2d040dd6307816e5206
Léo LE BRAS <leo.lebrasf@gmail.com>
2017-01-30 17:33:39 +0100

    feat(Link): rewrite

--- modules/Link.js
+++ modules/Link.js
@@ -1,12 +1,14 @@
 /* @flow */
 
-import React, { PropTypes, createElement } from 'react'
+import React, { PureComponent, PropTypes, createElement } from 'react'
 import { Platform, TouchableOpacity, TouchableNativeFeedback, View, Text } from 'react-native'
 import type { History } from './HistoryTypeDefinitions'
 
 type Props = {
-  children?: string,
-  to: string,
+  children?: string | (props: { onPress: () => void }) => React$Element<any>,
+  to?: string,
+  onPress?: () => void,
+  replace?: boolean,
   style?: StyleSheet,
   component?: () => React$Element<any>,
 }
@@ -15,21 +17,47 @@ type Context = {
   history: History,
 }
 
-const Link = (props: Props, context: Context): React$Element<any> => {
-  const Touchable = Platform.OS === 'ios'
-    ? TouchableOpacity
-    : TouchableNativeFeedback
-  const onPress = () => props.to && context.history.push(props.to)
-  return createElement(
-    props.component || Touchable,
-    { onPress },
-    <View style={props.style}>
-      <Text>{props.children}</Text>
-    </View>,
-  )
-}
-Link.contextTypes = {
-  history: PropTypes.object,
+class Link extends PureComponent<void, Props, void> {
+
+  props: Props
+  context: Context
+
+  static contextTypes = {
+    history: PropTypes.object,
+  }
+
+  onPress = (): void => {
+    const { replace, to, onPress } = this.props
+    const { history } = this.context
+    if (to) {
+      if (replace) {
+        history.replace(to)
+      } else {
+        history.push(to)
+      }
+    } else if (onPress) {
+      onPress()
+    }
+  }
+
+  render(): ?React$Element<any> {
+    const { component, style, children } = this.props
+    const Touchable = Platform.OS === 'ios'
+      ? TouchableOpacity
+      : TouchableNativeFeedback
+    if (!children) return null
+    if (typeof children === 'function') {
+      return children({ onPress: this.onPress })
+    }
+    return createElement(
+      component || Touchable,
+      { onPress: this.onPress },
+      <View style={style}>
+        <Text>{children}</Text>
+      </View>,
+    )
+  }
+
 }
 
 export default Link
