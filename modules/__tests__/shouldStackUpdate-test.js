447b9a29917b93a30d5570bfc8a12ff916541e87
Léo LE BRAS <leo.lebrasf@gmail.com>
2017-02-16 00:54:29 +0100

    tests(utils): move test file

--- modules/__tests__/shouldStackUpdate-test.js
+++ modules/__tests__/shouldStackUpdate-test.js
@@ -5,62 +5,66 @@ describe('shouldStackUpdate() util', () => {
   it('shouldStackUpdate() returns true when paths are different', () => {
     const currentRoute = { key: '/foo', path: '/foo' }
     const nextRoute = { key: '/bar', path: '/bar' }
-    const routerHistory = createHistory({
+    const currentRouterHistory = createHistory({
+      initialEntries: ['/foo'],
+    })
+    const nextRouterHistory = createHistory({
       initialIndex: 1,
       initialEntries: ['/foo', '/bar'],
     })
-    const previousLocation = { pathname: '/foo' }
-    const input = shouldStackUpdate(currentRoute, nextRoute, routerHistory, previousLocation)
+    const input = shouldStackUpdate(currentRoute, nextRoute, currentRouterHistory, nextRouterHistory)
     expect(input).toBeTruthy()
   })
   it('shouldStackUpdate() returns false when paths are equal', () => {
     const currentRoute = { key: '/foo', path: '/foo' }
     const nextRoute = { key: '/foo', path: '/foo' }
-    const routerHistory = createHistory({
+    const currentRouterHistory = createHistory({
+      initialEntries: ['/foo'],
+    })
+    const nextRouterHistory = createHistory({
       initialIndex: 0,
       initialEntries: ['/foo'],
     })
-    const previousLocation = { pathname: '/foo' }
-    const input = shouldStackUpdate(currentRoute, nextRoute, routerHistory, previousLocation)
+    const input = shouldStackUpdate(currentRoute, nextRoute, currentRouterHistory, nextRouterHistory)
     expect(input).toBeFalsy()
   })
   it('shouldStackUpdate() returns true when paths (with params) are equal and pathnames are different (1)', () => {
     const currentRoute = { key: '/article/:id', path: '/article/:id' }
     const nextRoute = { key: '/article/:id', path: '/article/:id' }
-    const routerHistory = {
-      ...createHistory({
-        initialIndex: 0,
-        initialEntries: ['/article/1', '/article/2'],
-      }),
-    }
-    const previousLocation = { pathname: '/article1' }
-    const input = shouldStackUpdate(currentRoute, nextRoute, routerHistory, previousLocation)
+    const currentRouterHistory = createHistory({
+      initialIndex: 1,
+      initialEntries: ['/article/1', '/article/2'],
+    })
+    const nextRouterHistory = createHistory({
+      initialEntries: ['/article/1', '/article/2'],
+    })
+    const input = shouldStackUpdate(currentRoute, nextRoute, currentRouterHistory, nextRouterHistory)
     expect(input).toBeTruthy()
   })
   it('shouldStackUpdate() returns true when paths (with params) are equal and pathnames are different (2)', () => {
     const currentRoute = { key: '/article/:id', path: '/article/:id' }
     const nextRoute = { key: '/article/:id', path: '/article/:id' }
-    const routerHistory = {
-      ...createHistory({
-        initialIndex: 1,
-        initialEntries: ['/article/1', '/article/2'],
-      }),
-    }
-    const previousLocation = { pathname: '/article1' }
-    const input = shouldStackUpdate(currentRoute, nextRoute, routerHistory, previousLocation)
+    const currentRouterHistory = createHistory({
+      initialEntries: ['/article1'],
+    })
+    const nextRouterHistory = createHistory({
+      initialIndex: 1,
+      initialEntries: ['/article/1', '/article/2'],
+    })
+    const input = shouldStackUpdate(currentRoute, nextRoute, currentRouterHistory, nextRouterHistory)
     expect(input).toBeTruthy()
   })
   it('shouldStackUpdate() returns false when paths (without params) are equal and pathname are different', () => {
     const currentRoute = { key: '/app', path: '/app' }
     const nextRoute = { key: '/app', path: '/app' }
-    const routerHistory = {
-      ...createHistory({
-        initialIndex: 1,
-        initialEntries: ['/app', '/app/article'],
-      }),
-    }
-    const previousLocation = { pathname: '/app' }
-    const input = shouldStackUpdate(currentRoute, nextRoute, routerHistory, previousLocation)
+    const currentRouterHistory = createHistory({
+      initialEntries: ['/app'],
+    })
+    const nextRouterHistory = createHistory({
+      initialIndex: 1,
+      initialEntries: ['/app', '/app/article'],
+    })
+    const input = shouldStackUpdate(currentRoute, nextRoute, currentRouterHistory, nextRouterHistory)
     expect(input).toBeFalsy()
   })
 })
