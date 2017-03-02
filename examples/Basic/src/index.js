0601bd2a5050f4824cd87d1c746cb6c2c55cdcd1
Léo LE BRAS <leo.lebrasf@gmail.com>
2017-03-02 14:57:36 +0100

    feat(deeplinking): add example

--- examples/Basic/src/index.js
+++ examples/Basic/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { StyleSheet, View, Text } from 'react-native'
 import { Switch, Route, Redirect } from 'react-router'
-import { NativeRouter, Link } from 'react-router-native'
+import { NativeRouter, Link, DeepLinking } from 'react-router-native'
 import { Navigation, Card, Tabs, Tab } from 'react-router-navigation'
 
 const styles = StyleSheet.create({
@@ -12,73 +12,76 @@ const styles = StyleSheet.create({
 
 export default () => (
   <NativeRouter>
-    <Navigation>
-      <Card
-        exact
-        path="/"
-        render={() => (
-          <View style={styles.container}>
-            <Text>Index</Text>
-            <Link to="/yolo">
-              <Text>Push to a new scene</Text>
-            </Link>
-          </View>
-        )}
-      />
-      <Card
-        path="/yolo"
-        component={() => (
-          <View style={styles.container}>
-            <Text>Yolo</Text>
-            <Link to="/hello/one">
-              <Text>Push to tabs</Text>
-            </Link>
-          </View>
-        )}
-        title="Yolo"
-      />
-      <Card
-        path="/hello"
-        title="Hello"
-        render={({ match: { url } }) => (
-          <Switch>
-            <Route
-              exact
-              path={url}
-              render={() => <Redirect to={`${url}/one`} />}
-            />
-            <Tabs style={styles.container}>
-              <Tab
-                path={`${url}/one`}
-                label="One"
-                render={() => (
-                  <View style={styles.container}>
-                    <Text>One</Text>
-                  </View>
-                )}
+    <View style={styles.container}>
+      <DeepLinking />
+      <Navigation>
+        <Card
+          exact
+          path="/"
+          render={() => (
+            <View style={styles.container}>
+              <Text>Index</Text>
+              <Link to="/yolo">
+                <Text>Push to a new scene</Text>
+              </Link>
+            </View>
+          )}
+        />
+        <Card
+          path="/yolo"
+          component={() => (
+            <View style={styles.container}>
+              <Text>Yolo</Text>
+              <Link to="/hello/one">
+                <Text>Push to tabs</Text>
+              </Link>
+            </View>
+          )}
+          title="Yolo"
+        />
+        <Card
+          path="/hello"
+          title="Hello"
+          render={({ match: { url } }) => (
+            <Switch>
+              <Route
+                exact
+                path={url}
+                render={() => <Redirect to={`${url}/one`} />}
               />
-              <Tab
-                path={`${url}/two`}
-                label="Two"
-                render={() => (
-                  <View style={styles.container}>
-                    <Text>Two</Text>
-                  </View>
-                )}
-              />
-              <Tab
-                path={`${url}/three`}
-                label="Three"
-                render={() => (
-                  <View style={styles.container}>
-                    <Text>Three</Text>
-                  </View>
-                )}
-              />
-            </Tabs>
-          </Switch>
-        )}
-      />
-    </Navigation>
+              <Tabs style={styles.container}>
+                <Tab
+                  path={`${url}/one`}
+                  label="One"
+                  render={() => (
+                    <View style={styles.container}>
+                      <Text>One</Text>
+                    </View>
+                  )}
+                />
+                <Tab
+                  path={`${url}/two`}
+                  label="Two"
+                  render={() => (
+                    <View style={styles.container}>
+                      <Text>Two</Text>
+                    </View>
+                  )}
+                />
+                <Tab
+                  path={`${url}/three`}
+                  label="Three"
+                  render={() => (
+                    <View style={styles.container}>
+                      <Text>Three</Text>
+                    </View>
+                  )}
+                />
+              </Tabs>
+            </Switch>
+          )}
+        />
+      </Navigation>
+    </View>
   </NativeRouter>
 )
