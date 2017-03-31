81d194ca59208ece84f9d0a1267661f4f169f3c8
Charles Mangwa <charles.mangwa-chuisse@hetic.net>
2017-04-01 00:43:16 +0200

    fix(TabBarBottom): center labels & icons vertically

--- modules/TabBarBottom.js
+++ modules/TabBarBottom.js
@@ -24,7 +24,7 @@ const styles = StyleSheet.create({
   },
   tab: {
     flex: 1,
-    justifyContent: 'flex-end',
+    justifyContent: 'center',
     paddingVertical: 4,
     height: TAB_HEIGHT,
   },
