784732858eb7a526de020473d17947219a8fdcc8
Léo LE BRAS <leo.lebrasf@gmail.com>
2018-01-06 11:57:22 +0100

    fix(minimal-app): update config + remove deep linking

--- examples/minimal-app/App.js
+++ examples/minimal-app/App.js
@@ -7,7 +7,7 @@ import {
   Text,
 } from 'react-native'
 import { Switch, Route, Redirect } from 'react-router'
-import { NativeRouter, Link, DeepLinking } from 'react-router-native'
+import { NativeRouter, Link } from 'react-router-native'
 import { Navigation, Card, Tabs, Tab } from 'react-router-navigation'
 
 const PRIMARY_COLOR = 'rgb(226, 68, 68)'
@@ -41,140 +41,145 @@ class App extends Component {
   render() {
     return (
       <NativeRouter>
-        <DeepLinking>
-          <View style={styles.container}>
-            <StatusBar barStyle={this.state.navigation.barStyle} />
-            <Navigation
-              navBarStyle={this.state.navigation.navBarStyle}
-              titleStyle={this.state.navigation.titleStyle}
-              backButtonTintColor={this.state.navigation.backButtonTintColor}
-            >
-              <Card
-                exact
-                path="/"
-                title="Index"
-                render={() => (
-                  <View style={styles.scene}>
-                    <Link component={TouchableOpacity} to="/yolo">
-                      <Text>Push a new scene</Text>
-                    </Link>
-                    <TouchableOpacity
-                      style={styles.button}
-                      onPress={() => {
-                        this.setState({
-                          navigation: {
-                            navBarStyle: { backgroundColor: PRIMARY_COLOR },
-                            titleStyle: { color: 'white' },
-                            barStyle: 'light-content',
-                            backButtonTintColor: 'white',
-                          },
-                        })
-                      }}
-                    >
-                      <Text>Change navbar style</Text>
-                    </TouchableOpacity>
-                  </View>
-                )}
-              />
-              <Card
-                titleStyle={this.state.card.titleStyle}
-                navBarStyle={this.state.card.navBarStyle}
-                backButtonTintColor={this.state.card.backButtonTintColor}
-                path="/yolo"
-                render={() => (
-                  <View style={styles.scene}>
-                    <Link component={TouchableOpacity} to="/hello">
-                      <Text>Push tabs</Text>
-                    </Link>
-                    <TouchableOpacity
-                      style={styles.button}
-                      onPress={() => {
-                        this.setState(prevState => ({
-                          navigation: {
-                            ...prevState.navigation,
-                            barStyle: 'light-content',
-                          },
-                          card: {
-                            ...prevState.card,
-                            navBarStyle: { backgroundColor: SECONDARY_COLOR },
-                            titleStyle: { color: 'white' },
-                            backButtonTintColor: 'white',
-                          },
-                        }))
-                      }}
-                    >
-                      <Text>Change navbar style</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity
-                      style={styles.button}
-                      onPress={() => {
-                        this.setState(prevState => ({
-                          card: {
-                            ...prevState.card,
-                            title: 'New title !',
-                          },
-                        }))
-                      }}
-                    >
-                      <Text>Change title</Text>
-                    </TouchableOpacity>
-                  </View>
-                )}
-                title={this.state.card.title || 'Yolo'}
-              />
-              <Card
-                path="/hello"
-                title="Hello"
-                render={({ match, location }) => (
-                  <Switch location={location}>
-                    <Route
-                      exact
-                      path={match.url}
-                      render={() => <Redirect to={`${match.url}/one`} />}
-                    />
-                    <Route
-                      render={() => (
-                        <Tabs
-                          style={styles.container}
-                          tabBarStyle={styles.tabs}
-                          tabBarIndicatorStyle={styles.indicator}
-                        >
-                          <Tab
-                            path={`${match.url}/one`}
-                            label="One"
-                            render={() => (
-                              <View style={styles.scene}>
-                                <Text>One</Text>
-                              </View>
-                            )}
-                          />
-                          <Tab
-                            path={`${match.url}/two`}
-                            label="Two"
-                            render={() => (
-                              <View style={styles.scene}>
-                                <Text>Two</Text>
-                              </View>
-                            )}
-                          />
-                          <Tab
-                            path={`${match.url}/three`}
-                            label="Three"
-                            render={() => (
-                              <View style={styles.scene}>
-                                <Text>Three</Text>
-                              </View>
-                            )}
-                          />
-                        </Tabs>
-                      )}
-                    />
-                  </Switch>
-                )}
-              />
-            </Navigation>
-          </View>
-        </DeepLinking>
+        <View style={styles.container}>
+          <StatusBar barStyle={this.state.navigation.barStyle} />
+          <Navigation
+            navBarStyle={this.state.navigation.navBarStyle}
+            titleStyle={this.state.navigation.titleStyle}
+            backButtonTintColor={this.state.navigation.backButtonTintColor}
+          >
+            <Card
+              exact
+              path="/"
+              title="Index"
+              render={() => (
+                <View style={styles.scene}>
+                  <Link component={TouchableOpacity} to="/yolo">
+                    <Text>Push a new scene</Text>
+                  </Link>
+                  <TouchableOpacity
+                    style={styles.button}
+                    onPress={() => {
+                      this.setState({
+                        navigation: {
+                          navBarStyle: { backgroundColor: PRIMARY_COLOR },
+                          titleStyle: { color: 'white' },
+                          barStyle: 'light-content',
+                          backButtonTintColor: 'white',
+                        },
+                      })
+                    }}
+                  >
+                    <Text>Change navbar style</Text>
+                  </TouchableOpacity>
+                </View>
+              )}
+            />
+            <Card
+              titleStyle={
+                this.state.card.titleStyle || this.state.navigation.titleStyle
+              }
+              navBarStyle={
+                this.state.card.navBarStyle || this.state.navigation.navBarStyle
+              }
+              backButtonTintColor={
+                this.state.card.backButtonTintColor ||
+                this.state.navigation.backButtonTintColor
+              }
+              path="/yolo"
+              render={() => (
+                <View style={styles.scene}>
+                  <Link component={TouchableOpacity} to="/hello">
+                    <Text>Push tabs</Text>
+                  </Link>
+                  <TouchableOpacity
+                    style={styles.button}
+                    onPress={() => {
+                      this.setState(prevState => ({
+                        navigation: {
+                          ...prevState.navigation,
+                          barStyle: 'light-content',
+                        },
+                        card: {
+                          ...prevState.card,
+                          navBarStyle: { backgroundColor: SECONDARY_COLOR },
+                          titleStyle: { color: 'white' },
+                          backButtonTintColor: 'white',
+                        },
+                      }))
+                    }}
+                  >
+                    <Text>Change navbar style</Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity
+                    style={styles.button}
+                    onPress={() => {
+                      this.setState(prevState => ({
+                        card: {
+                          ...prevState.card,
+                          title: 'New title !',
+                        },
+                      }))
+                    }}
+                  >
+                    <Text>Change title</Text>
+                  </TouchableOpacity>
+                </View>
+              )}
+              title={this.state.card.title || 'Yolo'}
+            />
+            <Card
+              path="/hello"
+              title="Hello"
+              render={({ match, location }) => (
+                <Switch location={location}>
+                  <Route
+                    exact
+                    path={match.url}
+                    render={() => <Redirect to={`${match.url}/one`} />}
+                  />
+                  <Route
+                    render={() => (
+                      <Tabs
+                        style={styles.container}
+                        tabBarStyle={styles.tabs}
+                        tabBarIndicatorStyle={styles.indicator}
+                      >
+                        <Tab
+                          path={`${match.url}/one`}
+                          label="One"
+                          render={() => (
+                            <View style={styles.scene}>
+                              <Text>One</Text>
+                            </View>
+                          )}
+                        />
+                        <Tab
+                          path={`${match.url}/two`}
+                          label="Two"
+                          render={() => (
+                            <View style={styles.scene}>
+                              <Text>Two</Text>
+                            </View>
+                          )}
+                        />
+                        <Tab
+                          path={`${match.url}/three`}
+                          label="Three"
+                          render={() => (
+                            <View style={styles.scene}>
+                              <Text>Three</Text>
+                            </View>
+                          )}
+                        />
+                      </Tabs>
+                    )}
+                  />
+                </Switch>
+              )}
+            />
+          </Navigation>
+        </View>
       </NativeRouter>
     )
   }
