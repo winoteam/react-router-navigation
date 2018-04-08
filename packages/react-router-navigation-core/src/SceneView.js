1f1221beda3866209ad3bb49ac11df6bba3e71f8
Léo LE BRAS <leo.lebrasf@gmail.com>
2018-04-08 12:29:13 +0200

    feat(react-router-navigation-core): move SceneView component

--- packages/react-router-navigation-core/src/SceneView.js
+++ packages/react-router-navigation-core/src/SceneView.js
@@ -1,40 +1,38 @@
 /* @flow */
 
-import React from 'react'
-import { matchPath } from 'react-router'
-import type { RouterHistory, Location, Match } from 'react-router'
-import type { RouteProps } from 'react-router-navigation-core'
+import * as React from 'react'
+import { matchPath, type RouterHistory, type Location, type Match } from 'react-router'
+import type { Route, RouteProps } from './TypeDefinitions'
 
-type Props = RouteProps & {
-  type: 'card' | 'tab',
-  history: RouterHistory,
-}
+type Props = Route &
+  RouteProps & {
+    history: RouterHistory,
+  }
 
-type State = {
+type State = {|
   location: Location,
   match: ?Match,
-}
+|}
 
 class SceneView extends React.Component<Props, State> {
   unlisten: ?Function
 
   constructor(props: Props) {
     super(props)
-    const { path, exact, strict, history: { location } } = props
-    const match = matchPath(location.pathname, { path, exact, strict })
-    this.state = { match, location }
+    const { history: { location }, routeMatch } = props
+    this.state = { match: routeMatch, location }
   }
 
   componentWillMount() {
     const { history } = this.props
-    this.unlisten = history.listen(this.onListenHistory)
+    this.unlisten = history.listen(this.onHistoryChange)
   }
 
   componentWillUnmount() {
     if (this.unlisten) this.unlisten()
   }
 
-  onListenHistory = (location: Location) => {
+  onHistoryChange = (location: Location) => {
     const { path, exact, strict } = this.props
     const match = matchPath(location.pathname, { path, exact, strict })
     if (!this.state.match) {
@@ -49,17 +47,15 @@ class SceneView extends React.Component<Props, State> {
   }
 
   render() {
-    // $FlowFixMe
-    const { render, children, component: Component, type, history } = this.props
+    const { render, children, component: Component, history } = this.props
     const { match, location } = this.state
-    if (type === 'card' && !match) return null
     const contextRouter = { history, match, location }
     if (render) {
       return render(contextRouter)
     } else if (children && typeof children === 'function') {
       return children(contextRouter)
     } else if (children && React.Children.count(children) === 0) {
-      return React.Children.only(children)
+      return React.cloneElement(children, contextRouter)
     } else if (Component) {
       return <Component {...contextRouter} />
     }
--- packages/react-router-navigation-core/src/index.js
+++ packages/react-router-navigation-core/src/index.js
@@ -3,10 +3,12 @@
 // High-level wrappers
 export { default as CardStack } from './CardStack'
 export { default as TabStack } from './TabStack'
+export { default as SceneView } from './SceneView'
 
 // Low-level building blocks
-export { shouldUpdate, get, createKey, getRoute, build } from './StackUtils'
-export { runHistoryListenner } from './HistoryUtils'
+export { default as RouteUtils } from './RouteUtils'
+export { default as StackUtils } from './StackUtils'
+export { default as StateUtils } from './StateUtils'
 
 // Type definitions
 export type {
